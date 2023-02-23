949c7372710b5a993313857bb3149312e79efc91
Poly <124129346+PolyUstinova@users.noreply.github.com>
2023-02-23 23:36:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+"use strict";
+
+let content = document.querySelector('#content');
+let cards = document.querySelector('.cards');
+let headCat = document.querySelector('#head-cat');
+let wrapper = document.querySelector('.wrapper');
+
+let categoryList = document.querySelectorAll('.category-sub');
+let currentCategory = "";
+let headCatText = "";
+for(let li of categoryList){
+    li.addEventListener('click', function(){
+        currentCategory = li.dataset.category;
+        headCatText = li.textContent;
+        getProductsByCategory(currentCategory);
+    }
+)};
+
+function showProduct(data){
+    let allProducts = data.products;
+    headCat.textContent = headCatText;
+    let productList = "";
+    for (const product of allProducts) {
+        productList += `
+        <div class="product-card" id="${product.id}">
+            <div class="info">
+            <img src="${product["images"][0]}" alt="" class="img-product">
+                <div class="info-card">
+                <p class="title-product">${product.title}</p>
+                <p class="price-product">$${product.price}</p>
+                </div>
+            <p class="desc-product">${product.description}</p>
+            </div>
+            <input type="button" value="Купить" class="buy-btn buy-card-btn"></input>
+        </div>`;
+    }
+    cards.innerHTML = productList;
+    let productCards = document.querySelectorAll('.product-card');
+    for(let card of productCards){
+        card.addEventListener('click', function (e){
+            if(e.target.type == "button"){
+                console.log('buy');
+            } else {
+                let productId;
+                for(const pr of allProducts){
+                    if (pr.id == card.id){
+                        productId = pr.id;
+                    }
+                }
+                fetch('https://dummyjson.com/products/' + productId)
+                .then(res => res.json())
+                .then(res => showInfoAboutProduct(res));
+                e.preventDefault();
+            }
+        });
+    }
+}
+
+function getProductsByCategory(category){
+    if(category == "all"){
+        fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(res => showProduct(res));
+    } else {
+        fetch('https://dummyjson.com/products/category/' + category)
+        .then(res => res.json())
+        .then(data => showProduct(data));
+    }
+}
+
+
+function showInfoAboutProduct(product){
+    let popUpBg = document.querySelector('.popup-bg');
+    let pop = `<div class="product-popup">
+        <svg class="close-popup" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#8D2FF7" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>
+        <div class="main-info">
+            <img src="${product["images"][0]}" alt="" class="photo-pop">
+            <div class="main-txt">
+                <p class="title-pop">${product.title}</p>
+                <div class="rating-pop">
+                    <img src="img/Star 1.png" alt="" class="star-img">
+                    <p class="rating-pop-txt">${product.price}</p>
+                </div>
+                <p class="price">$<span class="price-pop">${product.price}</span></p>
+                <input type="button" value="Купить" class="buy-btn pop-btn"></input>
+            </div>
+        </div>
+        <div class="about-product">
+            <p class="head-about">About product</p>
+            <p class="desc-pop">${product.description}</p>
+            <p>Brand: <span class="brand-pop"></span>${product.brand}</p>
+        </div>
+    </div>`;
+    popUpBg.innerHTML = pop;
+    let productPopUp = document.querySelector('.product-popup');
+    let closePopUpBtn = document.querySelector('.close-popup');
+    popUpBg.classList.add('active');
+    productPopUp.classList.add('active');
+    closePopUpBtn.addEventListener('click', function(){
+        popUpBg.classList.remove('active');
+        productPopUp.classList.remove('active');
+    });
+
+    document.addEventListener('click', function(e){
+        if(e.target == popUpBg){
+            popUpBg.classList.remove('active');
+            productPopUp.classList.remove('active');
+        }
+    });
+}
