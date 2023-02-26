0f87cd15db85d05dbc05111f736ab22a97b8818b
Poly <124129346+PolyUstinova@users.noreply.github.com>
2023-02-26 19:02:05 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,10 @@
+let content = document.querySelector('#content');
+let cards = document.querySelector('.cards');
+let headCat = document.querySelector('#head-cat');
+let wrapper = document.querySelector('.wrapper');
+let submenu = document.querySelector('.submenu');
+let popUpBg = document.querySelector('.popup-bg');
+
+let categoryList = document.querySelectorAll('.category-sub');
+let currentCategory = "";
+let headCatText = "";
